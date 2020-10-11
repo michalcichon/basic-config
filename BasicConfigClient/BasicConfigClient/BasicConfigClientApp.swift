f3a3dcc9a09b08f546804c733d2d6d617cf623a8
Michał Cichoń <michal.cichon@codete.com>
2020-10-11 14:31:34 +0200

    Initial commit

---
+++ BasicConfigClient/BasicConfigClient/BasicConfigClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicConfigClientApp.swift
+//  BasicConfigClient
+//
+//  Created by Michał Cichoń on 11/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BasicConfigClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
