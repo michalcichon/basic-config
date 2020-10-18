7af551cada64e7053b48abed5469330ccc28245b
Michał Cichoń <michal.cichon@codete.com>
2020-10-18 18:18:23 +0200

    wip

--- BasicConfigClient/BasicConfigClient/ContentViewModel.swift
+++ BasicConfigClient/BasicConfigClient/ContentViewModel.swift
@@ -10,5 +10,5 @@ import UIKit
 class ContentViewModel: ObservableObject {
     var buttonText = ConfigService.shared.string(key: "app.fun_button.text", defaultValue: "Test me!")
     var buttonURL = ConfigService.shared.url(key: "app.fun_button.url", defaultValue: URL(string: "https://www.google.com")!)
-    var backgroundColor = ConfigService.shared.color(key: "app.background.color", defaultValue: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) 
+    var backgroundColor = ConfigService.shared.color(key: "app.background.color", defaultValue: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
 }
