c448675433037211d9d3b38b89373e90116e3157
Michał Cichoń <michal.cichon@codete.com>
2020-10-18 16:57:43 +0200

    small rename

--- BasicConfigServer/server.js
+++ BasicConfigServer/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to basic config application." });
+  res.json({ message: "Welcome to basic config server." });
 });
 
 // routes
