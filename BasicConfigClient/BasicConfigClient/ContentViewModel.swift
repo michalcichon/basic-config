54abcd876d0bab188553f415fabd4c12989aabb0
Michał Cichoń <michal.cichon@codete.com>
2020-10-18 19:18:04 +0200

    fix update with computed properties

--- BasicConfigClient/BasicConfigClient/ContentViewModel.swift
+++ BasicConfigClient/BasicConfigClient/ContentViewModel.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class ContentViewModel: ObservableObject {
     
-    var buttonText = ConfigService.shared.string(key: "app.fun_button.text", defaultValue: "Test me!")
-    var buttonURL = ConfigService.shared.url(key: "app.fun_button.url", defaultValue: URL(string: "https://www.google.com")!)
-    var backgroundColor = ConfigService.shared.color(key: "app.background.color", defaultValue: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    var buttonText: String { ConfigService.shared.string(key: "app.fun_button.text", defaultValue: "Test me!") }
+    var buttonURL: URL { ConfigService.shared.url(key: "app.fun_button.url", defaultValue: URL(string: "https://www.google.com")!) }
+    var backgroundColor: UIColor { ConfigService.shared.color(key: "app.background.color", defaultValue: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) }
     
     init() {
         NotificationCenter.default.addObserver(self, selector: #selector(onConfigUpdate), name: .didUpdateConfig, object: nil)
